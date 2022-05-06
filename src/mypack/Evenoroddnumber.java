83fbd97f00adb8b0cebb93653e46fc5f245bd379
Reena <Reenapatel.qa@gmail.com>
2022-05-06 11:51:21 -0400

    First Commit with java class, variables, data types and operators

---
+++ src/mypack/Evenoroddnumber.java
@@ -0,0 +1,20 @@
+package mypack;
+
+public class Evenoroddnumber {
+
+	public static void main(String[] args) {
+		
+		int a=30 ;
+		int b=20;
+		
+		if(a>b)
+		{
+			System.out.println("a is largest");
+		}
+		else
+		{
+			System.out.println("b is largest");
+		}
+	}
+
+}
---
+++ src/mypack/Relationaloperator.java
@@ -0,0 +1,21 @@
+package mypack;
+
+public class Relationaloperator {
+
+	public static void main(String[] args) {
+		
+		int a = 30;
+		int b = 20;
+		
+		// Relational(Comparison operators  --> ==, <,>, <=, >=, != //
+		// Always return boolean value//
+		
+		System.out.println(a==b);
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(a!=b);
+	}
+
+}

ce61be4f888dd6da1e0eea84be19ad663316d5cb
Reena <Reenapatel.qa@gmail.com>
2022-06-07 15:50:14 -0400

    Operators and Condition statements committed

---
+++ src/largestoftwonumber/Largestoftwonumbers.java
@@ -0,0 +1,19 @@
+package largestoftwonumber;
+
+public class Largestoftwonumbers {
+
+	public static void main(String[] args) {
+		
+		int num = 7;
+		
+		if(num%2 == 0)
+		{
+			System.out.println("Number is even number");
+		}
+		else
+		{
+			System.out.println("Number is odd number");
+		}
+	}
+
+}
